59f4423c7cd8f6b64ae318da045fd544d06fb5b2
Brendon Walsh <brendonwalsh@niamu.com>
2024-04-20 23:06:05 -0400

    Render JSON and EDN output for ST1-3

--- simulator/src/dcg/simulator/rules_parser.clj
+++ simulator/src/dcg/simulator/rules_parser.clj
@@ -1,5 +1,6 @@
 (ns dcg.simulator.rules-parser
   (:require
+   [clojure.data.json :as json]
    [clojure.java.io :as io]
    [clojure.string :as string]
    [instaparse.core :as insta]))
@@ -23,9 +24,6 @@
                       :in [$]
                       :where [[?c :card/image ?i]
                               [?c :card/parallel-id 0]
-                              (or [?c :card/effect _]
-                                  [?c :card/inherited-effect _]
-                                  [?c :card/security-effect _])
                               [?c :card/number ?n]
                               [?i :image/language "en"]]}
                     (d/db db/conn))
@@ -52,5 +50,72 @@
      :total (+ (get result false 0)
                (get result true 0))}
     (get m true))
+
+  (defn json
+    [x]
+    (with-open [out (io/writer (io/file "resources/cards.json"))]
+      (json/write (doall x)
+                  out
+                  :value-fn (fn [k v]
+                              (if (uri? v)
+                                (str v)
+                                v))
+                  :indent true
+                  :escape-slash false
+                  :escape-unicode false)))
+
+  (->> (d/q '{:find [[(pull ?c [:card/id
+                                :card/name
+                                :card/number
+                                :card/category
+                                :card/parallel-id
+                                :card/level
+                                :card/dp
+                                :card/play-cost
+                                :card/use-cost
+                                :card/language
+                                :card/form
+                                :card/attribute
+                                :card/type
+                                :card/rarity
+                                :card/notes
+                                {:card/color [:color/index
+                                              :color/color]}
+                                {:card/digivolve-conditions [:digivolve/index
+                                                             :digivolve/color
+                                                             :digivolve/cost]}
+                                {:card/releases [:release/name
+                                                 :release/genre
+                                                 :release/date]}
+                                {:card/image [:image/source
+                                              :image/language]}
+                                {:card/highlights [:highlight/index
+                                                   :highlight/field
+                                                   :highlight/type
+                                                   :highlight/text]}
+                                :card/effect
+                                :card/inherited-effect
+                                :card/security-effect]) ...]]
+              :in [$]
+              :where [[?c :card/image ?i]
+                      [?c :card/parallel-id 0]
+                      [?c :card/number ?n]
+                      (or [(clojure.string/starts-with? ?n "ST1-")]
+                          [(clojure.string/starts-with? ?n "ST2-")]
+                          [(clojure.string/starts-with? ?n "ST3-")])
+                      [?i :image/language "en"]]}
+            (d/db db/conn))
+       (pmap (fn [{:card/keys [effect inherited-effect security-effect] :as card}]
+               (let [process-fn (fn [text]
+                                  {:text/raw text
+                                   :text/tree (->> (parse text)
+                                                   (apply concat)
+                                                   (into []))})]
+                 (-> (cond-> card
+                       effect (update :card/effect process-fn)
+                       inherited-effect (update :card/inherited-effect process-fn)
+                       security-effect (update :card/security-effect process-fn))))))
+       (into [])
+       #_json)
 
   )
